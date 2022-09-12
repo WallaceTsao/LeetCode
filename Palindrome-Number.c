0abb9a75fcb11388fed9bd1603b8e200736133cb
WallaceTsao <113355803+WallaceTsao@users.noreply.github.com>
2022-09-12 20:49:30 +0800

    Create Palindrome-Number.c

---
+++ Palindrome-Number.c
@@ -0,0 +1,16 @@
+bool isPalindrome(int x) {
+    
+    int revert = 0;
+    
+    if (x < 0 || (x % 10 == 0 && x != 0))
+    {
+        return false;
+    }
+    
+    while (x > revert) {
+        revert = revert * 10 + x % 10;
+        x /= 10;
+    }
+    
+    return x == revert || x == revert / 10;
+}
